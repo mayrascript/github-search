fec7da0e0eb95a6ccdae554a45afba6be8e69645
Mayra <mayrarodriguez1709@gmail.com>
2020-05-31 18:35:15 -0500

    chore: .env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   baseUrl: 'https://api.github.com',
   githubAuthUrl: 'https://github.com/login/oauth/authorize',
-  clientId: '1474872cbeecc4172bdf',
-  clientSecret: '2132bd94a8efde7aa8fbadaa5e9c0592619c160d',
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
   callbackUrl: 'http://localhost:4200/auth',
 };
